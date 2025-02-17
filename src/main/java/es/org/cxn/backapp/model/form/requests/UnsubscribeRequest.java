fae170ade2f1df72ad2a160eccb57b0835377fdb
Santiago Paz <santi93.09@gmail.com>
2025-02-17 11:50:30 +0100

    add validation not null password

--- src/main/java/es/org/cxn/backapp/model/form/requests/UnsubscribeRequest.java
+++ src/main/java/es/org/cxn/backapp/model/form/requests/UnsubscribeRequest.java
@@ -12,10 +12,10 @@ package es.org.cxn.backapp.model.form.requests;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,6 +27,7 @@ package es.org.cxn.backapp.model.form.requests;
  */
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 /**
@@ -36,6 +37,7 @@ import jakarta.validation.constraints.Size;
  */
 public record UnsubscribeRequest(
 
+        @NotNull
         @NotBlank(message = ValidationConstants.PASSWORD_NOT_BLANK_MESSAGE)
         @Size(min = ValidationConstants.PASSWORD_MIN_LENGTH, max = ValidationConstants.PASSWORD_MAX_LENGTH,
                 message = ValidationConstants.PASSWORD_SIZE_MESSAGE) String password) {
